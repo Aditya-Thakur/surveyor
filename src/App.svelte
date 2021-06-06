22455cbaece081803d357db13dc3c52cdf1b14df
Aditya <adityathakur532@gmail.com>
2021-06-06 14:31:26 +0530

    Skeleton

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	let counter = 0;
+</script>
+
+<main>
+	<button on:click="{() => {counter++;}}">I have been clicked {counter} times.</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
