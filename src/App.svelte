7d32125948f97e585eb42a3ef42635aa6c3e4fa8
Aditya <adityathakur532@gmail.com>
2021-07-17 21:31:29 +0530

    navbar updated

--- src/App.svelte
+++ src/App.svelte
@@ -1,22 +1,88 @@
 <script lang="ts">
-	let counter = 0;
+	import {
+		navOptionsLoggedIn,
+		navOptionsLoggedOut,
+	} from "./shared/Nav.svelte";
+	let selected = navOptionsLoggedIn[2];
+	let intSelected = 2;
+	function changeComponent(id) {
+		selected = navOptionsLoggedIn[id];
+		intSelected = id;
+	}
 </script>
+<body>
+	
 
-<main>
-	<button on:click="{() => {counter++;}}">I have been clicked {counter} times.</button>
-</main>
-
+<!-- <div class="container"> -->
+<ul class="navbar">
+	{#each navOptionsLoggedIn as option, i}
+		<div
+			class={intSelected == i ? "nav-link active" : "nav-link inactive"}
+			on:click={() => {
+				changeComponent(i);
+			}}
+			role="tab"
+		>
+			{option.page}
+		</div>
+	{/each}
+</ul>
+<div class="row">
+	<div class="col-sm-12">
+		<div class="p-2">
+			<svelte:component this={selected.component} />
+		</div>
+	</div>
+</div>
+</body>
+<!-- </div> -->
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+	.navbar {
+		display: flex;
+		align-items: center;
+		justify-content: left;
+		background-color: teal;
+		color: white;
+		height: 40px;
+		border-bottom-left-radius: 10px;
+		border-bottom-right-radius: 10px;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	.nav-link {
+		padding: 6px 10px;
+		font-size: large;
+		margin: 3px 6px;
 	}
+
+	.nav-link:hover,
+	.active {
+		background-color: white;
+		color: rgb(21, 80, 80);
+		border-top-left-radius: 15px;
+		border-top-right-radius: 15px;
+		margin-bottom: -6px;
+		cursor: pointer;
+	}
+
+	.inactive {
+		background-color: white;
+		color: teal;
+		border-bottom-left-radius: 15px;
+		border-bottom-right-radius: 15px;
+		margin-top: -7px;
+	}
+
+	:root {
+    --bg-color: rgba(10, 116, 116, 0.151);
+    --primary-color: rgb(255, 255, 255);
+    --secondary-color:rgb(41, 255, 244);
+    --nav-bg-color: rgb(49, 14, 0);
+  }
+
+
+body {
+    width: 100%;
+    height: 100%;
+    background-color: var(--bg-color);
+}
 </style>
---
+++ src/shared/Nav.svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    import CreateSurvey from "../Components/CreateSurvey.svelte";
+    import Dashboard from "../Components/Dashboard.svelte";
+    import Home from "../Components/Home.svelte";
+    import HowToUse from "../Components/HowToUse.svelte";
+
+    export const navOptionsLoggedIn = [
+        { page: "Home", component: Home },
+        { page: "How To Use", component: HowToUse },
+        { page: "Create Survey", component: CreateSurvey },
+        { page: "Dashboard", component: Dashboard },
+    ];
+    export const navOptionsLoggedOut = [
+        { page: "Home", component: Home },
+        { page: "Create Survey", component: CreateSurvey },
+        { page: "Your Surveys", component: Dashboard },
+    ];
+</script>
