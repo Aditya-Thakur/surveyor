59f41032361bd1ca043320b57e76c51f0d8807b5
Aditya <adityathakur532@gmail.com>
2021-07-18 17:26:25 +0530

    store updated

--- src/store.ts
+++ src/store.ts
@@ -3,37 +3,39 @@ import { writable } from "svelte/store";
 export var current = writable(null);
 
 export const environment = {
-    production: false,
-    backendUrl: 'https://surveyor-be.vercel.app/',
-    frontEndUrl: 'https://surveyor-ui.vercel.app/',
-    questionCategories: [ {
-        name: "Short Answer",
-        alias: "SA",
-        icon: "short_text"
-      },{
-        name: "Long Answer",
-        alias: "LA",
-        icon: "view_headline"
-      }, {
-        name: "Multiple Choice",
-        alias: "MC",
-        icon: "radio_button_checked"
-      },{
-        name: "Checkboxes",
-        alias: "CB",
-        icon: "check_box"
-      },{
-        name: "Rating",
-        alias: "R",
-        icon: "star_half"
-      }
-    ],
-    currentUser : {
-      id: '',
-      fullName : '',
-      email : '',
-      gender : '',
-      dob : new Date(),
-      mobileNumber : ''
+  production: false,
+  backendUrl: 'https://surveyor-be.vercel.app/',
+  frontEndUrl: 'https://surveyor-ui.vercel.app/',
+  questionCategories: {
+    "SA" : {
+      name: "Short Answer",
+      icon: "short_text"
+    },
+    "LA" : {
+      name: "Long Answer",
+      icon: "view_headline"
+    },
+    "MC" : {
+      name: "Multiple Choice",
+      icon: "radio_button_checked"
+    },
+
+    "CB" : {
+      name: "Checkboxes",
+      icon: "check_box"
+    }, 
+
+    "RT" : {
+      name: "Rating",
+      icon: "star_half"
     }
-  };
+  },
+  currentUser : {
+    id: '',
+    fullName : '',
+    email : '',
+    gender : '',
+    dob : new Date(),
+    mobileNumber : ''
+  }
+};
